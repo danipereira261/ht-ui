ff4054adff2e63f2ccc9d76bc6f2971a82131b15
Daniele Pereira <dpereira261@gmail.com>
2022-06-13 09:35:17 -0300

    config server

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require("path");
 const app = express();
 
 // Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
-app.use(express.static(__dirname + "/dist/ht-io"));
+app.use(express.static(__dirname + "/dist/ht-ui"));
 
 app.get("/*", function (req, res) {
     res.sendFile(path.join(__dirname + "/dist/ht-ui/index.html"));
