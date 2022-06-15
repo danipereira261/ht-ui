eca49df91a6ea0375dd40fb1359992bb06c5d331
Daniele Pereira <dpereira261@gmail.com>
2022-06-15 13:15:04 -0300

    ajuste rota

--- src/service/peso.service.ts
+++ src/service/peso.service.ts
@@ -11,17 +11,14 @@ export class PessoaService {
     }
 
     public cadastrar(model: PesoModel) {
-        // return this.http.post(`http://localhost:8080/api/v1/registro-peso`, model);
         return this.http.post(`https://ht-app-back.herokuapp.com/api/v1/registro-peso` , model);
     }
 
     public buscarTodosRegistros(cpf: string) {
-        // return this.http.get('http://localhost:8080/api/v1/registro-peso/?cpf='  + cpf);
         return this.http.get('https://ht-app-back.herokuapp.com/api/v1/registro-peso/?cpf=' + cpf);
     }
 
     public deletById(id: number ) {
-        // return this.http.delete('http://localhost:8080/api/v1/registro-peso/?id=' + id);
         return this.http.delete('https://ht-app-back.herokuapp.com/api/v1/registro-peso/?id=' + id);
     }
 }
